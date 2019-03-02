4185263da5df1d04967ebc0f13a8f1b98058fc21
Timothy Shores <timothymshores@gmail.com>
2019-03-01 20:31:38 -0600

    Refactored React components into nested components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        );
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
@@ -23,11 +36,25 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                <p>Placeholder: Options will be displayed here</p>
+                <Option />
+                <Option />
+                <Option />
             </div>
         );
     }
 }
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Placeholder: A unique and specific option will be displayed here</p>
+            </div>
+        );
+    }
+}
+
+
 
 class AddOption extends React.Component {
     render() {
@@ -39,13 +66,4 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-        <Header />
-        <Action />
-        <Options />
-        <AddOption />
-    </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
